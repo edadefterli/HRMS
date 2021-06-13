2876a251451b604c534da66184842c6149cf25d3
edadefterli <edadefterli@github.com>
2021-06-13 17:51:53 +0300

    added swagger

---
+++ hrms/src/main/java/kodlama/io/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,12 @@
+package kodlama.io.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService {
+	
+	List<JobPosition> getAll();
+	
+
+}
